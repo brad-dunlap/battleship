9a1df0261b40139d04b71754806062e7c6e836f3
Brad Dunlap <braddunlapdpt@gmail.com>
2022-12-06 16:38:56 -0700

    refactor: formatting

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,10 @@
 class Ship
-    attr_reader :name,
-                :length
-    def initialize(name, length)
-        @name = name
-        @length = length
-    end
+	attr_reader :name,:length
+							
+	def initialize(name, length)
+		@name = name
+    @length = length
+	end
 end
+
+
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,10 +1,10 @@
 require 'RSpec'
 require './lib/ship'
 require 'pry'
+
 RSpec.describe Ship do
-    it 'exists' do
-        cruiser = Ship.new("Cruiser", 3)
-        expect(cruiser).to be_an_instance_of(Ship)
-        require 'pry'; binding.pry
-    end
+	it 'exists' do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser).to be_an_instance_of(Ship)
+  end
 end
