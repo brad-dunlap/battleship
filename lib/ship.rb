e8f0666bd2710b3c025d944797ce92bc8404c7b6
Brad Dunlap <braddunlapdpt@gmail.com>
2022-12-06 18:56:25 -0700

    feat: Ship can sink

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,14 +1,22 @@
 class Ship
-    attr_reader :name,
-                :length
-								:health
-    def initialize(name, length)
-        @name = name
-        @length = length
-				@health = length
-    end
-
-		def sunk?
-        
+	attr_reader :name,
+              :length
+							:health
+  def initialize(name, length)
+      @name = name
+      @length = length
+			@health = health
 		end
+
+	def health
+		health = length
+	end
+
+	def sunk?
+		if health == 0
+			true
+		else
+			false
+		end       
+	end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,20 +2,19 @@ require 'RSpec'
 require './lib/ship'
 require 'pry'
 RSpec.describe Ship do
-    it 'exists' do
-        cruiser = Ship.new("Cruiser", 3)
+		let(:cruiser) { Ship.new("Cruiser", 3)}
+
+    it 'initializes' do
         expect(cruiser).to be_an_instance_of(Ship)
     end
     
     it 'has attributes' do
-        cruiser = Ship.new("Cruiser", 3)
         expect(cruiser.name).to eq("Cruiser")
         expect(cruiser.length).to eq(3)
         expect(cruiser.health).to eq(3)
     end
 
     it 'has sunk?' do
-        cruiser = Ship.new("Cruiser", 3)
         expect(cruiser.sunk?).to eq(false)
     end
 end
