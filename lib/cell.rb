28610d22629348f30897a125e2451bcac2a5076f
Danae Mata <danae.mata1515@gmail.com>
2022-12-06 23:09:01 -0500

    feat: cell can be fired upon

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,32 @@
 class Cell
-    attr_reader :coordinate
+    attr_reader :coordinate,
+                :ship,
+                :fired_upon
 
-    def initialize (coordinate)
+    def initialize(coordinate, fired_upon = false)
         @coordinate = coordinate
+        @ship = ship
+        @fired_upon = fired_upon
+    end
+
+    def empty?
+        !@ship
+    end
+
+    def place_ship(ship)
+        @ship = ship
+    end
+
+    def fired_upon? 
+        if fired_upon == true
+        true
+       else
+        false
+       end
+    end
+
+    def fire_upon
+        ship.hit
+        @fired_upon = true
     end
 end
