baab2013da8423b04afd7016b58227b386d82c22
Brad Dunlap <braddunlapdpt@gmail.com>
2022-12-08 11:59:29 -0700

    feat: update valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -48,6 +48,7 @@ require './lib/board'
 		expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
 		expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
 		expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+		expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
 		expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
 
 	end
